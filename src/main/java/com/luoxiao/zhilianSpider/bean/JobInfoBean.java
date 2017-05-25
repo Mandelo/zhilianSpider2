025cfb2846f78c09e61ab7b16d945fca1ad19ea1
luoxiao <loshower@gmail.com>
2017-05-25 19:50:58 +0800

    优化

--- src/main/java/com/luoxiao/zhilianSpider/bean/JobInfoBean.java
+++ src/main/java/com/luoxiao/zhilianSpider/bean/JobInfoBean.java
@@ -1,60 +1,67 @@
 package com.luoxiao.zhilianSpider.bean;
 
-/** 
-* @Description: 职位信息bean
-* @author luoxiao
-* @date 2017年3月25日 
-*/
+/**
+ * @Description: bean
+ * @author losho
+ * @date 2017年5月25日
+ */
 public class JobInfoBean {
-    //公司名
-	private String companyName;
-	//网址
-	private String url;
-	//工资待遇/月
-	private String wages;
-	//公司地点
-	private String location;
-	//发布日期
-	private String date;
-	
-	public String getCompanyName() {
-		return companyName;
-	}
-	public void setCompanyName(String companyName) {
-		this.companyName = companyName;
-	}
-	public String getUrl() {
-		return url;
-	}
-	public void setUrl(String url) {
-		this.url = url;
-	}
-	public String getWages() {
-		return wages;
-	}
-	public void setWages(String wages) {
-		this.wages = wages;
-	}
-	public String getLocation() {
-		return location;
-	}
-	public void setLocation(String location) {
-		this.location = location;
-	}
-	public String getDate() {
-		return date;
-	}
-	public void setDate(String date) {
-		this.date = date;
-	}
-	@Override
-	public String toString() {
-		return "JobInfoBean [ companyName=" + companyName
-				+ ", wages=" + wages + ", location="
-				+ location + ", date=" + date + "]";
-	}
-	
-	
-	
+
+    // 公司名
+    private String companyName;
+    // 网址
+    private String url;
+    // 工资待遇/月
+    private String wages;
+    // 公司地点
+    private String location;
+    // 发布日期
+    private String date;
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getWages() {
+        return wages;
+    }
+
+    public void setWages(String wages) {
+        this.wages = wages;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "JobInfoBean [ companyName=" + companyName + ", wages=" + wages + ", location=" + location + ", date="
+                + date + "]";
+    }
 
 }
